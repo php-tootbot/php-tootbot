b1e4c768d755a95d62b1a432273a225763dc1b4a
smiley <codemasher@users.noreply.github.com>
2023-08-02 00:47:42 +0200

    :octocat: +media uploader

--- src/TootBot.php
+++ src/TootBot.php
@@ -14,6 +14,7 @@ use chillerlan\HTTP\Common\MultipartStreamBuilder;
 use chillerlan\HTTP\Psr17\RequestFactory;
 use chillerlan\HTTP\Psr17\StreamFactory;
 use chillerlan\HTTP\Psr18\CurlClient;
+use chillerlan\HTTP\Utils\MessageUtil;
 use chillerlan\OAuth\Core\AccessToken;
 use chillerlan\OAuth\Providers\Mastodon;
 use chillerlan\OAuth\Storage\MemoryStorage;
@@ -24,8 +25,11 @@ use Psr\Http\Client\ClientInterface;
 use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\StreamFactoryInterface;
+use Psr\Http\Message\StreamInterface;
 use Psr\Log\LoggerInterface;
 use Throwable;
+use function array_map;
+use function in_array;
 use function random_bytes;
 use function sha1;
 use function sleep;
@@ -74,6 +78,20 @@ abstract class TootBot implements TootBotInterface{
 	}
 
 	/**
+	 * initializes a PSR-17 request factory
+	 */
+	protected function initRequestFactory():RequestFactoryInterface{
+		return new RequestFactory;
+	}
+
+	/**
+	 * initializes a PSR-17 stream factory
+	 */
+	protected function initStreamFactory():StreamFactoryInterface{
+		return new StreamFactory;
+	}
+
+	/**
 	 * initializes a PSR-18 http client
 	 */
 	protected function initHTTP():ClientInterface{
@@ -95,27 +113,6 @@ abstract class TootBot implements TootBotInterface{
 			->setStorage(new MemoryStorage)
 			->storeAccessToken(new AccessToken($tokenParams))
 		;
-	}
-
-	/**
-	 * initializes a PSR-17 request factory
-	 */
-	protected function initRequestFactory():RequestFactoryInterface{
-		return new RequestFactory;
-	}
-
-	/**
-	 * initializes a PSR-17 stream factory
-	 */
-	protected function initStreamFactory():StreamFactoryInterface{
-		return new StreamFactory;
-	}
-
-	/**
-	 * returns a new multipart stream builder
-	 */
-	protected function getMultipartStreamBuilder():MultipartStreamBuilder{
-		return new MultipartStreamBuilder($this->streamFactory);
 	}
 
 	/**
@@ -162,6 +159,89 @@ abstract class TootBot implements TootBotInterface{
 			$this->submitTootFailure($response);
 		}
 
+	}
+
+	/**
+	 * Uploads the image content given in the StreamInterface, returns the media id required for embedding, null on error.
+	 *
+	 * @see https://docs.joinmastodon.org/methods/media/
+	 * @see https://docs.joinmastodon.org/api/guidelines/#focal-points
+	 */
+	protected function uploadMedia(
+		StreamInterface $image,
+		string          $description,
+		string          $filename,
+		StreamInterface $thumbnail = null,
+		array           $focus = null
+	):?string{
+		// create the multipart body
+		$multipartStreamBuilder = new MultipartStreamBuilder($this->streamFactory);
+
+		// the description/alt-text
+		$multipartStreamBuilder->addString(
+			content  : $description,
+			fieldname: 'description',
+			headers  : ['Content-Encoding' => 'UTF-8']
+		);
+
+		// optional focal points
+		if($focus !== null){
+			$focus = array_map('floatval', $focus);
+
+			$multipartStreamBuilder->addString(
+				content  : sprintf('%f,%f', $focus[0], $focus[1]),
+				fieldname: 'focus',
+			);
+		}
+
+		// the image content stream
+		$multipartStreamBuilder->addStream(
+			stream   : $image,
+			fieldname: 'file',
+			filename : $filename,
+			headers  : ['Content-Transfer-Encoding' => 'binary']
+		);
+
+		// an optional thumbnail - the file type should be the same as the image as the MIME is determined by the given filename
+		if($thumbnail !== null){
+			$multipartStreamBuilder->addStream(
+				stream   : $thumbnail,
+				fieldname: 'thumbnail',
+				filename : 'thumbnail-'.$filename,
+				headers  : ['Content-Transfer-Encoding' => 'binary']
+			);
+		}
+
+		// create and fire the upload request
+		$request = $this->requestFactory
+			->createRequest('POST', $this->options->instance.'/api/v2/media')
+			->withProtocolVersion('1.1')
+			->withHeader('User-Agent', $this->options->user_agent);
+
+		/** @var \Psr\Http\Message\RequestInterface $request */
+		$request        = $multipartStreamBuilder->build($request);
+		/** @phan-suppress-next-line PhanTypeMismatchArgument */
+		$uploadResponse = $this->mastodon->sendRequest($request);
+		$status         = $uploadResponse->getStatusCode();
+
+		if(!in_array($status, [200, 202], true)){
+			$this->logger->error(sprintf('image upload error: HTTP/%s', $status));
+
+			return null;
+		}
+
+		try{
+			$json = MessageUtil::decodeJSON($uploadResponse);
+		}
+		catch(Throwable $e){
+			$this->logger->error(sprintf('image upload response json decode error: %s', $e->getMessage()));
+
+			return null;
+		}
+
+		$this->logger->info(sprintf('upload successful, media id: "%s"', $json->id));
+
+		return (string)$json->id;
 	}
 
 	/**
